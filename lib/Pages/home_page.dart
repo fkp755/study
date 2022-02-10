a9b1902231a9a103ad241f015d244b49a6585b86
Faisalkhan Pathan <fkp755@gmail.com>
2022-02-10 21:51:27 +0530

    Chapter 4 flutter List view builder, fetching data from network, reading json format

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
-import '../change_name_card.dart';
 import '../drawer.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -11,11 +12,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var myText = "Change me";
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
   TextEditingController _nameController = TextEditingController();
   @override
   void initState() {
-
     super.initState();
+    // getdata(); //Called to load data directly
+  }
+  getdata() async{
+     var res = await http.get(Uri.parse(url));
+     data = jsonDecode(res.body);
+     print(data);
+     setState(() {});
   }
 
   @override
@@ -25,18 +34,31 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("School"),
       ),
-      body:Padding(
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: ChangeNameCard(myText: myText, nameController: _nameController),
-          ),
-        ),
+        child: data != null
+            ? ListView.builder(
+                itemBuilder: (context, index){
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      subtitle: Text("ID: ${data[index]["id"]}"),
+                      leading: Image.network(data[index]["url"]),
+                    ),
+                  );
+                },
+              itemCount: data.length,
+            )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           myText = _nameController.text;
+          getdata(); //called to load data on on click of floating button
           setState(() {});
         },
         child: Icon(Icons.refresh_outlined),
--- lib/drawer.dart
+++ lib/drawer.dart
@@ -14,6 +14,7 @@ class MyDrawer extends StatelessWidget {
             accountName: Text("Faisalkhan"), accountEmail: Text("fkp755@gmail.com"),
             currentAccountPicture: CircleAvatar(
               backgroundImage: NetworkImage("https://images.unsplash.com/photo-1630763882488-73a2d2267c0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
+
             ),
           ),
           ListTile(
